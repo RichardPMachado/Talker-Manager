0de90fc165469437c86e493ee5698597f4d63bfa
RichardPMachado <richardmachado2211@gmail.com>
2022-12-18 16:50:38 -0300

    feat[requisito 1]: Criar o endpoint GET /talker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { getAllUser } = require('./utils/handleUser');
 
 const app = express();
 app.use(express.json());
@@ -8,7 +9,12 @@ const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(HTTP_OK_STATUS).send();  
+});
+
+app.get('/talker', async (_request, response) => {
+  const users = await getAllUser(); 
+  response.status(HTTP_OK_STATUS).json(users);
 });
 
 app.listen(PORT, () => {
---
+++ src/utils/handleUser.js
@@ -0,0 +1,14 @@
+const { readFile, writeFile } = require('fs').promises;
+const path = require('path');
+
+const userPath = path.resolve(__dirname, '..', 'talker.json');
+
+const getAllUser = async () => {
+  const response = await readFile(userPath, 'utf-8');
+  const users = JSON.parse(response);
+  return users;
+};
+
+module.exports = {
+  getAllUser,
+};
